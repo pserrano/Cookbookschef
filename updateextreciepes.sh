9402524c321e580700b6c1d4d96ef3d212f399a2
Pablo Serrano <pserrano@genexies.net>
2013-06-04 15:25:31 +0200

    add commit update & sleep routines

--- updateextreciepes.sh
+++ updateextreciepes.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
-#updateextreciepes.sh 1.1 by Pablo Serrano 
- 
+#updateextreciepes.sh 1.2 by Pablo Serrano
+# Current : add commit update & sleep routines
+# 1.1 : fix variables bad syntax
+# 1.0 : Initial version  
+
 #You can add repo name for all public cookbook on http://github.com/opscode-cookbooks
 APPS=(tomcat java openssl apache2)
 
 echo "Deleting old dirs..."
 
 for i in "${APPS[@]}"; do rm -fr ${i}; done 
+sleep 2
 
 echo "Refresh repos..." 
 
 for i in "${APPS[@]}"; do git clone git://github.com/opscode-cookbooks/${i}; done
+
+sleep 2
 
 echo "deleting crap for git ignores.."
 
@@ -21,4 +27,15 @@ for i in "${APPS[@]}"
 		rm -fr .gitignore .git/
 		cd ..
 	done
+
+sleep 2
+
+echo "add updates, commit and push..."
+
+git add .
+git commit -m "Refresh repos"
+git push
+
+sleep 2
+
 echo "finish"
