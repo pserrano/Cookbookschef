deaee97aa201ddfdb68acaadc32a4319cfe85c16
Pablo Serrano <pserrano@genexies.net>
2013-06-04 15:02:48 +0200

    add updateextreciepes script

---
+++ updateextreciepes.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+APPS=(tomcat java openssl apache2)
+
+echo "Deleting old dirs..."
+
+for i in "${APPS[@]}"; do rm -fr $i{i}; done 
+
+echo "Refresh repos..." 
+
+for i in "${APPS[@]}"; do git clone git://github.com/opscode-cookbooks/${i}; done
+
+echo "deleting crap for git ignores.."
+
+for i in "${APPS[@]}" 
+	do 
+		cd ${i}
+		rm -fr .gitignore .git/
+		cd ..
+	done
+echo "finish"
